d61409adfad57608c94eb047644f9e29b46ee979
Evan Yu <50347938+Badbird5907@users.noreply.github.com>
2026-05-29 04:10:17 -0400

    helppp

--- scripts/ci/vercel-deploy.sh
+++ scripts/ci/vercel-deploy.sh
@@ -3,5 +3,9 @@ pushd .
 cd ./packages/backend
 pnpm run convex deploy --cmd "pnpm run build:app"
 pnpm run convex env set --preview-name $VERCEL_GIT_COMMIT_REF SITE_URL $VERCEL_URL
+echo "AEHJNE: apps/tanstack-start/.vercel/output"
+ls -la apps/tanstack-start/.vercel/output
+echo "FESJNHF: apps/tanstack-start/.vercel"
+ls -la apps/tanstack-start/.vercel
 popd
 cd ./apps/tanstack-start
